e3366145b4bb6c84235ede5741a3ad04e0967904
msudrajat <82055672+msudrajat@users.noreply.github.com>
2021-04-14 15:09:52 -0700

    Update App.py

--- App.py
+++ App.py
@@ -1,4 +1,5 @@
 import plotly.graph_objs as go
+import plotly.express as px
 import pandas as pd
 import dash
 import dash_core_components as dcc
@@ -12,10 +13,10 @@ df1 = df[df['Activity Description'] == 'Billable'].sort_values('Week', ascending
 
 # Dash Deployment
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
-server = app.server
+Server = app.server
 
 app.layout = html.Div([
-    dbc.Row(dbc.Col(html.H3("Trending Billable Hours (TTW)"),
+    dbc.Row(dbc.Col(html.H3("Billable Hours"),
                     width={'size': 6, 'offset': 5},
                     ),
             ),
@@ -35,6 +36,16 @@ app.layout = html.Div([
                     width=4, lg={'size': 6, "offset": 0, 'order': 'last'}
                     ),
         ]
+    ),
+    dbc.Row(
+        [
+            dbc.Col(dcc.Graph(id='Sunburst 2', figure={}),
+                    width=8, lg={'size': 6, "offset": 0, 'order': 'first'}
+                    ),
+            dbc.Col(dcc.Graph(id='Sunburst', figure={}),
+                    width=4, lg={'size': 6, "offset": 0, 'order': 'last'}
+                    ),
+        ]
     )
 
 ])
@@ -42,7 +53,9 @@ app.layout = html.Div([
 
 @app.callback(
     [Output(component_id='Utilization Graph', component_property='figure'),
-     Output(component_id='Utilization Graph 2', component_property='figure')],
+     Output(component_id='Utilization Graph 2', component_property='figure'),
+     Output(component_id='Sunburst', component_property='figure'),
+     Output(component_id='Sunburst 2', component_property='figure')],
     [Input(component_id='Select Group', component_property='value')])
 def update_graph(employee_name_value):
     L1 = df1[df1['Employee Group'] == 'SUP'].groupby(['Week End']).sum().tail(12)
@@ -59,19 +72,28 @@ def update_graph(employee_name_value):
     Figl.add_trace(go.Scatter(x=L5.index, y=L5['tc unit'], mode='lines', name='MCR', connectgaps=True))
 
     Figl.update_layout(hovermode='x unified')
-    Figl.update_layout(title='By Group', paper_bgcolor='lightsteelblue')
+    Figl.update_layout(title='Trending Billable By Group (TTW)', paper_bgcolor='lightsteelblue')
     Figl['layout']['xaxis']['autorange'] = "reversed"
 
     S1 = df1[df1['last name'] == employee_name_value].groupby(['Week End']).sum().tail(12)
+    DFS = df1[df1['last name'] == employee_name_value]
 
     Figs = go.Figure()
     Figs.add_trace(go.Scatter(x=S1.index, y=S1['tc unit'], mode='lines', name=employee_name_value, connectgaps=True))
 
     Figs.update_layout(hovermode='x unified')
-    Figs.update_layout(title='By Employee', paper_bgcolor='lightsteelblue')
+    Figs.update_layout(title='Trending Billable By Employee (TTW)', paper_bgcolor='lightsteelblue')
     Figs['layout']['xaxis']['autorange'] = "reversed"
 
-    return Figl, Figs
+    Figb = px.sunburst(df1, path=['Day', 'Employee Group'], values='tc unit',
+                       color='Day')
+    Figb.update_layout(title='Group Billable Hours Breakdown by Day', paper_bgcolor='lightsteelblue')
+
+    Figa = px.sunburst(DFS, path=['Day'], values='tc unit',
+                       color='Day')
+    Figa.update_layout(title='Employee Billable Hours Breakdown by Day', paper_bgcolor='lightsteelblue')
+
+    return Figl, Figs, Figb, Figa
 
 
 if __name__ == '__main__':
